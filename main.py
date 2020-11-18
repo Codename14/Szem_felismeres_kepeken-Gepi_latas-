a11a1f29d78f7f04286dfa3b29d0de624623dc28
Codename14 <46088872+Codename14@users.noreply.github.com>
2020-11-18 21:37:08 +0100

    New files added

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import cv2
 
-# So in this line of code we have loaded the image, you need to have an image in your working directory
-image = cv2.imread("real_00034.jpg")
+#loaded the image, you need to have an image in your working directory
+image = cv2.imread("face_dataset/real_00044.jpg.")
 blurImg = cv2.blur(image,(30,30))
 #gausBlur = cv2.GaussianBlur(image, (5,5),0)
 
 
-# This is for loading our Haar Cascade Classifier that we have already copied in our directory
+#loading our Haar Cascade Classifier
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-#face_cascade = cv2.CascadeClassifier("own_cascade.xml") #trained by 128 positive image, 270 negative image
-
-
-# detectMultiScale() function is for detecting objects if it finds a face in the image it will return
-# in the form of x,y,w,h. and it needs some parameters.
-# This is parameter is for specifying  how much the image size is reduced at each image scale.
-# minNeighbors: Parameter specifying how many neighbors each candidate rectangle should have to retain it,
-# this parameter will affect the quality of the detected faces.
+#face_cascade = cv2.CascadeClassifier("OwnFaceCascade_size24x24_stages_15.xml") #trained by 128 positive image, 270 negative image
+#face_cascade = cv2.CascadeClassifier("OwnFaceCascade_size24x24_stages_16.xml") #trained by 128 positive image, 270 negative image
+#face_cascade = cv2.CascadeClassifier("OwnFaceCascade_size24x24_stages_17.xml") #trained by 128 positive image, 270 negative image
+#face_cascade = cv2.CascadeClassifier("OwnFaceCascade_size24x24_stages_18.xml") #trained by 128 positive image, 270 negative image
+#face_cascade = cv2.CascadeClassifier("OwnFaceCascade_size24x24_stages_19.xml") #trained by 128 positive image, 270 negative image
+#face_cascade = cv2.CascadeClassifier("OwnFaceCascade_size24x24_stages_20.xml") #trained by 128 positive image, 270 negative image
+#face_cascade = cv2.CascadeClassifier("OwnFaceCascade_size32x32_stages_14.xml") #trained by 128 positive image, 270 negative image
 
 quit = False
 i = 1
@@ -40,47 +39,48 @@ while  quit == False and i < 200 and len(eyes) == 2:
     print(f"Arc mennyisége: {len(faces)}")
     if len(faces) == 1:
         quit = True
-    if len(eyes) == 0:
-            quit = True
+    if len(faces) == 0:
+        quit = True
     i+=1
 
 #DEBUG
     # print(f"arc {faces}")
     # print(f"szem {eyes}")
-    # print(f"szem2 arcx{faces[0,0]} < szemx{eyes[1,0]} VAGY arcy{faces[0,1]} > szemy{eyes[1,1]}")
-    # print(f"szem  arcx{faces[0,0]} < szemx{eyes[0,0]} VAGY arcy{faces[0,1]} > szemy{eyes[0,1]}")
+    # print(f"Bal szem arcx{faces[0,0]} < szemx{eyes[1,0]} VAGY arcy{faces[0,1]} > szemy{eyes[1,1]}")
+    # print(f"Jobb szem  arcx{faces[0,0]} < szemx{eyes[0,0]} VAGY arcy{faces[0,1]} > szemy{eyes[0,1]}")
     # print(f"arcx{[faces[0,0] + faces[0,2]]} < szemx{[eyes[0,0] + eyes[0,2]]} VAGY arcy{[faces[0,1] + faces[0,3]]} < szemy{[eyes[0,0] + eyes[0,3]]}")
     # print(f"arcx{[faces[0,0] + faces[0,2]]} < szemx{[eyes[1,0] + eyes[1,2]]} VAGY arcy{[faces[0,1] + faces[0,3]]} < szemy{[eyes[1,0] + eyes[1,3]]}")
 
-blurry = False
+notreal = False
 
 if  len(eyes) > 0 and len(faces) > 0 :
-    #SZEM 1
-        #  x        x           és  y               y
-        #KEZDŐ
+    # Jobb szem
+     # Starting point
+     # x kordináta x kordináta  vagy  y kordináta y kordináta
     if faces[0,0] > eyes[0,0] or faces[0,1] > eyes[0,1]:
-        blurry = True
-        print("belep1")
-        #VÉG
+        notreal = True
+        print("Hibás pozíció 1")
+    # End point
     if [faces[0,0] + faces[0,2]] < [eyes[0,0] + eyes[0,2]] or [faces[0,1] + faces[0,3]] < [eyes[0,0] + eyes[0,3]]:
-        blurry = True
-        print("belep2")
-    #SZEM 2
-    # KEZDŐ
+        notreal = True
+        print("Hibás pozíció 2")
+    # Bal szem
+    # Starting point
     if faces[0, 0] > eyes[1, 0] or faces[0, 1] > eyes[1, 1]:
-        blurry = True
-        print("belep3")
+        notreal = True
+        print("Hibás pozíció 3")
 
-        # VÉG
+    # End point
     if [faces[0, 0] + faces[0, 2]] < [eyes[1, 0] + eyes[1, 2]] or [faces[0, 1] + faces[0, 3]] < [eyes[1, 0] + eyes[1, 3]]:
-        blurry = True
-        print("belep4")
+        notreal = True
+        print("Hibás pozíció 4")
 
-if len(eyes) != 2 or len(faces) != 1 or blurry:
+ 
+if len(eyes) != 2 or len(faces) != 1 or notreal:
     # font
     font = cv2.FONT_HERSHEY_SIMPLEX
     # org
-    org = (30, 350)
+    org = (40, 320)
     # fontScale
     fontScale = 1.2
     # Blue color in BGR
